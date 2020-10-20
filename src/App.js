0ef2b53f6ed24eed30a68a5f9b7d582222cb9a89
Martin Hajas <martin.hajas97@gmail.com>
2020-10-20 10:54:19 +0300

    Added general terms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Products from './Components/products'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Card, Button, CardTitle, CardText, Row, Col, CardImg } from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Products />
+        <Row>
+     <Col md="2" lg="2">
+          <Card body outline engine="primary">
+          <img height="120px" width="240px" src={require("./Images/1.jpg")} />
+             <CardTitle>Title</CardTitle>
+              <Button type="button">Button</Button>
+          </Card>
+     </Col>
+        </Row>
+
+        <Row>
+            {this.state.products.map((product, index) => (
+            <Col key={product.id}  md="2" lg="2">
+              <Card body outline engine="primary">
+               <img height="120px" width="240px" src={require("./Images/" + product.img)}/>
+               <CardTitle>{product.name}</CardTitle>
+               <Button type="button" id={product.id} onClick=   {this.handleClick}>Button</Button>
+              </Card>
+            </Col>
+              ))}
+
+        </Row>
     </div>
+    
   );
 }
 
